783aefcc6bd9d44b84051880d393211ba62ef891
Jongmin Kim <jongmin.kim@legitgames.net>
2024-08-22 12:09:16 -0400

    add cache

--- lib/screen/main/post_detail/provider/product_post_provider.dart
+++ lib/screen/main/post_detail/provider/product_post_provider.dart
@@ -1,10 +1,12 @@
 
 
 
+import 'package:fast_app_base/common/dart/extension/num_duration_extension.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../data/network/daangn_api.dart';
 
 final productPostProvider = AutoDisposeFutureProviderFamily(<ProductPost, int>(ref, id) async {
+  ref.cacheFor(3.minutes);
   return await DaangnApi.getPost(id);
 });
